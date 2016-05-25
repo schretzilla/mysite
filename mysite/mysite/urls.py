8e37bac892b1fd2ebe4a4c411e19ee3b93a35355
schretzilla <schretzilla@gmail.com>
2016-05-25 16:00:07 -0400

    home redirects to blog

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include('blog.urls'))
+
 ]
