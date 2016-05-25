310b20c8a6c9af5bf2acf4b68f7db71d8390eb09
schretzilla <schretzilla@gmail.com>
2016-05-25 17:13:25 -0400

    removed urls from url pattern

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', include('blog.urls'))
+    #url(r'^blog/', include('blog.urls')),
+    #url(r'^admin/', admin.site.urls),
+    #url(r'^$', include('blog.urls'))
 
 ]
